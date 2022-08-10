f126aadee3cc41e06119668fac57432c6330d599
Adrian Strozzi <adrianstrozzi@gmail.com>
2022-08-09 22:53:32 -0500

    Updated for Heroku deploy test 3

--- server/server.js
+++ server/server.js
@@ -23,10 +23,6 @@ app.use(express.json());
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
-
-  pp.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-  });
 }
 
 // app.use(routes);
