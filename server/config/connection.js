37d8c016ed7ceacafe5dd3817467ddf28d8d7720
Adrian Strozzi <adrianstrozzi@gmail.com>
2022-08-09 22:45:30 -0500

    Updated for Heroku deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/booksearchengine',
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/booksearchengine',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
